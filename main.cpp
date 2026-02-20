d83920791d3f97c596c224eadf25d44fc266b02b
7zz-zz <stevenz0705@gmail.com>
2026-02-20 22:49:12 +0800

    second push: i push this code in clion

--- main.cpp
+++ main.cpp
@@ -3,5 +3,13 @@
 int main()
 {
     std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
